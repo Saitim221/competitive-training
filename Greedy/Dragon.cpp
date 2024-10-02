192ad89899e8856b628bc839f8c89f3ca6bbd4b1
Saitim221 <santinocirico@gmail.com>
2024-10-02 00:11:57 -0300

    Added Greedy problems

--- Greedy/Dragon.cpp
+++ Greedy/Dragon.cpp
@@ -15,7 +15,7 @@ Solution From Santino C.
 #define pb push_back
 #define fst first
 #define snd second
-#define fore(i,a,b) for(int i = 0;i < b; i++)
+#define fore(i,a,b) for(int i = a;i < b; i++)
 #define SZ(x) ((int)x.size())
 #define ALL(x) x.begin(), x.end()
 #define mset(a,v) memset((a), (v), sizeof(a))
@@ -24,3 +24,34 @@ using namespace std;
 typedef long long ll;
 
 
+
+void solve(vector<pair<ll,ll>> d, ll n, ll s){
+    bool alive = true;
+    int i = 0;
+    while(alive && i < n){
+        if(s > d[i].fst){
+            s += d[i].snd;
+        }else{
+            alive = false;
+        }
+        i++;
+    }
+    if(alive){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+}
+
+
+int main(){FIN;
+ll s, n; cin>>s>>n;
+vector<pair<ll,ll>> d(n); 
+for(auto &e:d) cin>>e.fst>>e.snd;
+sort(d.begin(), d.end());
+
+
+solve(d, n, s);
+
+
+}
